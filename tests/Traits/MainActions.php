6d943d248b5ffda3dbcf3fcff9f25658c9d9f3a4
Victor Allen <vamuigua@gmail.com>
2020-10-13 14:24:52 +0300

    update: Added Feature tests for Tenants
    The tests check for authentication rights before logging into the application
    The tenant gets redirected back to login page if not authenticated

--- tests/Traits/MainActions.php
+++ tests/Traits/MainActions.php
@@ -5,6 +5,7 @@ namespace Tests\Traits;
 use App\Role;
 use App\User;
 use App\Admin;
+use App\Tenant;
 
 trait MainActions
 {
@@ -25,6 +26,26 @@ trait MainActions
         $user = User::latest()->first();
         factory(Admin::class)->create(['user_id' => $user->id]);
         $user->roles()->attach(Role::where('name', 'Admin')->first());
+        return $user;
+    }
+
+    // generates an User Role
+    public function generateUserRole()
+    {
+        $role_user = new Role();
+        $role_user->name = 'User';
+        $role_user->description = 'A User';
+        $role_user->save();
+    }
+
+    // generates a User with Role User
+    public function generateUserWithRoleUser()
+    {
+        $this->generateUserRole();
+        $this->actingAs(factory(User::class)->create());
+        $user = User::latest()->first();
+        factory(Tenant::class)->create(['user_id' => $user->id]);
+        $user->roles()->attach(Role::where('name', 'User')->first());
         return $user;
     }
 }
