3622b2930b4d79553b0bff5a5abd6349e24372ae
Victor Allen <vamuigua@gmail.com>
2020-11-04 15:21:52 +0300

    update: Updated Featured listings UI
    Featured listings now show a random image selected from the uploaded images of a House Ad
    Removed the featured listings on the main page
    Changed the confirmation and vaidation urls for mpesa

--- app/Http/Controllers/Mpesa/MpesaController.php
+++ app/Http/Controllers/Mpesa/MpesaController.php
@@ -43,8 +43,8 @@ class MpesaController extends Controller
 
         $access_token = $this->access_token();
         $shortCode = '600610';
-        $confirmationUrl = 'https://60dd4b08340e.ngrok.io/api/confirm';  // remember to make urls https and use ngrok
-        $validationUrl = 'https://60dd4b08340e.ngrok.io/api/validate';
+        $confirmationUrl = 'https://0422dac9f660.ngrok.io/api/confirm';  // remember to make urls https and use ngrok
+        $validationUrl = 'https://0422dac9f660.ngrok.io/api/validate';
 
         $curl = curl_init();
         curl_setopt($curl, CURLOPT_URL, $url);
